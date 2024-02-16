9bf7a61692b118a6e50f71d07344c16278142f49
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-02-16 13:13:39 +0400

    Added rulesFormsApi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use App\Models\File;
@@ -21,9 +22,45 @@ class HomeController extends Controller
     {
         return view('pages.faq');
     }
+
     public function rulesForms()
     {
         $files = File::where('show', '1')->get();
         return view('pages.rules-forms', compact('files'));
     }
+
+    public function rulesFormsApi()
+    {
+        try {
+            $files = File::where('show', '1')->get();
+            $pdf = [];
+            foreach ($files as $file) {
+                $pdf[] = [
+                    'name' => $file->name,
+                    'rules-pdf' => asset($file->rules),
+                    'forms-pdf' => asset($file->forms),
+                    'fees-pdf' => asset($file->fees),
+                ];
+            }
+
+            if (empty($pdf)) {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'No files found'
+                ], 404);
+            }
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Successfully retrieved files',
+                'data' => $pdf
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Internal Server Error'
+            ], 500);
+        }
+    }
+
 }
