6cfcabc3134374388b4961518a71ce766d7ab9be
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-03-19 17:38:33 +0400

    Fixed bugs

--- resources/views/pages/user-single.blade.php
+++ resources/views/pages/user-single.blade.php
@@ -12,7 +12,7 @@
                 <div class="user-main-title-container">
                     <a href="{{route('cases.index')}}" class="case-number"><img src="../images/back-icon.png" alt="back icon"></a>
                     <div class="left-side">
-                        <h1 class="user-main-title">CASE-123456</h1>
+                        <h1 class="user-main-title">{{$case->number}}</h1>
                         <span class="paragraph">Case Overview & Details</span>
                     </div>
                 </div>
